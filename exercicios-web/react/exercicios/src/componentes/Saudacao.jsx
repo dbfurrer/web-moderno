bb27d9abbb4732f4f0ed9c4eeaeb1834509d873c
dbfurrer <dbfurrer@gmail.com>
2023-03-31 15:07:06 -0300

    fim dos exercícios React

--- exercicios-web/react/exercicios/src/componentes/Saudacao.jsx
+++ exercicios-web/react/exercicios/src/componentes/Saudacao.jsx
@@ -2,36 +2,35 @@ import React, { Component } from 'react'
 
 export default class Saudacao extends Component {
 
-    state = {
-        tipo: this.props.tipo,
-        nome: this.props.nome
-    }
-    
-    constructor(props) {
-        super(props)
-        
-        this.setTipo = this.setTipo.bind(this)
-    }
+  state = {
+    tipo: this.props.tipo,
+    nome: this.props.nome
+  }
+  
+  constructor(props) {
+    super(props)
 
-    setTipo(e) {
-        this.setState({ tipo: e.target.value })
-    }
+    this.setTipo = this.setTipo.bind(this) //bind atribui a mesma função à instância atual da classe criada
+  }
 
-    setNome(e) {
-        this.setState({ nome: e.target.value })
-    }
+  setTipo(e) {
+    this.setState({ tipo: e.target.value })    
+  }
 
-    render() {
-        const { tipo, nome } = this.state
-        return (
-            <div>
-                <h1>{tipo} {nome}!</h1>
-                <hr />
-                <input type="text" placeholder="Tipo..." 
-                    value={tipo} onChange={this.setTipo} />
-                <input type="text" placeholder="Nome..."
-                    value={nome} onChange={e => this.setNome(e)} />
-            </div>
-        )
-    }
+  setNome(e) {
+    this.setState({ nome: e.target.value })
+  }
+
+  render() {
+    const { tipo, nome } = this.state
+    return (
+      <div>
+        <h1>{tipo} {nome}!</h1>
+        <input type="text" placeholder="Tipo..." 
+          value={tipo} onChange={this.setTipo} />
+        <input type="text" placeholder= "Nome..." // nesse caso o this passado a função é chamada no onchange
+          value={nome} onChange={e => this.setNome(e)} /> 
+      </div>
+    )
+  }
 }
