befc9197c2abf59d9acbaaef68a9e03ac1bd9a2a
dbfurrer <dbfurrer@gmail.com>
2023-04-30 18:52:07 -0300

    correção no projeto react

--- exercicios-web/nextjs/fundamentos/.next/static/chunks/webpack.js
+++ exercicios-web/nextjs/fundamentos/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e4f664ad10f72ffd"; }
+/******/ 		__webpack_require__.h = function() { return "23837e113495bca8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
